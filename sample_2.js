90083a7027f3c6895fa988a89b393b58535f6f14
luciand <lucian.diaconescu@vectorwatch.com>
2015-09-16 09:42:57 +0300

    Eliminate token from samples for security reasons

--- sample_2.js
+++ sample_2.js
@@ -3,7 +3,7 @@ var sample_stream = require('./index.js');
 /*************Custom code**********/
 var counter = 0;
 var array = [];
-var token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1c2VySURcIjoxLFwicm9sZVwiOlwiQURNSU5cIixcIndhdGNoSWRcIjotMX0iLCJpc3MiOiJodHRwczpcL1wvdmVjdG9yd2F0Y2guY29tIiwiaWF0IjoxNDI0OTU2OTYxfQ.cyea19E8qrVojO7kz05Ybah-kKrdIl8sXSZZaDgFW6w';
+var token = '***';
 function updateAll() {
     array.push(sample_stream.packageRequestForData(counter++, ""));
     sample_stream.sendDeliverRequests(array);
