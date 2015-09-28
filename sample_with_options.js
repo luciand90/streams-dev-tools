41c287092de05735ac9e63f8dae47fdf54335260
luciand <lucian.diaconescu@vectorwatch.com>
2015-09-28 15:31:35 +0300

    Clean sample

--- sample_with_options.js
+++ sample_with_options.js
@@ -1,14 +1,12 @@
 var sample_stream = require('./index.js');
 var configJSON = {
-    streamUID: "24673c3e7f44534ce255d2ddc8460817",
+    streamUID: "***",
     streamType: "public",
     hasSettings: true,
-    token: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1c2VySURcIjoxLFwicm9sZVwiOlwiQURNSU5cIixcIndhdGNoSWRcIjotMX0iLCJpc3MiOiJodHRwczpcL1wvdmVjdG9yd2F0Y2guY29tIiwiaWF0IjoxNDQwMDc0MTYyfQ.U_aMr1YSnrQuN9UqdIyc7wfcDSheqp0Acy_Zo3EzyRQ",
+    token: "***",
     portNumber: "3000"
 };
-//////TODO test register/unregister!!!!!!!!!!!!!!!!
 sample_stream.config(configJSON);
-//sample_stream.debugMode = true;
 
 /*************Custom code**********/
 var counter = 0;
