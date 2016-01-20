ad352eecb468b9693dd09b5f7782f7def31d6656
Lucian Andrei Diaconescu <lucian.diaconescu@vectorwatch.com>
2016-01-20 19:22:31 +0200

    Change no content status code to 904

--- index.js
+++ index.js
@@ -356,7 +356,7 @@ var productionBaseURL = 'https://endpoint.vector.watch/VectorCloud/rest/',
             promise.then(function (streamValue) {
                 var httpStatus = 200;
                 if (typeof streamValue == 'object') {
-                    httpStatus = 204;
+                    httpStatus = 904;
                     streamValue = streamValue.msg;
                 }
                 privateMethods.log("Registration successfull, the response containing '" + streamValue + "' is being sent", LogLevels.info, true, _this.logstash);
